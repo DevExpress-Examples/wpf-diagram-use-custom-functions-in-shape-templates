ba68755bf16b72e9f7c6070f9331b7dd90cb044c
AndreySlabov <andrey.slabov@devexpress.com>
2023-08-11 11:01:00 +0400

    format

--- CS/WpfApp13/App.xaml.cs
+++ CS/WpfApp13/App.xaml.cs
@@ -6,12 +6,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace WpfApp13
-{
+namespace WpfApp13 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
-    {
+    public partial class App : Application {
     }
 }
--- CS/WpfApp13/MainWindow.xaml.cs
+++ CS/WpfApp13/MainWindow.xaml.cs
@@ -1,28 +1,9 @@
 ﻿using DevExpress.Data.Filtering;
 using DevExpress.Diagram.Core;
-using DevExpress.Utils.Serializing;
-using DevExpress.Xpf.Diagram;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace WpfApp13
-{
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+namespace WpfApp13 {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
